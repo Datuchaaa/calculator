8c45a533416a5f186d87761329baa741016fe5b5
Daviti Vashakmadze <vashaymadzedavit@gmail.com>
2024-01-23 23:35:14 +0400

    feat: add theme option switch  function with theme change

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-const calcDisplay = document.querySelector(".cacl-display")
-const numbersBtns = document.querySelectorAll(".num")
-const resetBtn = document.querySelector(".reset")
-const themeSwitchBtn = document.querySelector(".theme-switch-btn")
-const ovalBtn = document.querySelector(".oval-btn")
+const calcDisplay = document.querySelector(".calc-display");
+const numbersBtns = document.querySelectorAll(".num");
+const resetBtn = document.querySelector(".reset");
+const themeSwitchBtn = document.querySelector(".theme-switch-btn");
+const ovalBtn = document.querySelector(".oval-btn");
 
 let currentInput = "";
-let counter = 1
+let counter = 1;
 
-  numbersBtns.forEach(button => {
-    button.addEventListener("click", () => {
-      currentInput += button.textContent;
-      calcDisplay.textContent = currentInput;
-    });
+numbersBtns.forEach(button => {
+  button.addEventListener("click", () => {
+    currentInput += button.textContent;
+    calcDisplay.textContent = currentInput;
   });
+});
 
 resetBtn.addEventListener("click", () => {
-    calcDisplay.textContent = "0"
-    currentInput = ""
-    console.log("Ddddd")
-})
+  calcDisplay.textContent = "0";
+  currentInput = "";
+  console.log("Reset");
+});
 
 themeSwitchBtn.addEventListener("click", () => {
-
-  console.log("Swith")
-  if ( counter === 3) {
-    counter = 0
+  console.log("Switch");
+  
+  if (counter === 3) {
+    counter = 0;
   }
-  if ( counter < 3 ) {
-    ovalBtn.style.transform = `translateX(${counter * 22}px)`
-    document.body.classList.add("dark")
-    counter++
-    
-  } 
 
-})
+  if (counter === 0) {
+    ovalBtn.style.transform = `translateX(${counter * 22}px)`;
+    document.body.classList.remove("dark", "lilac");
+  } else if (counter === 1) {
+    ovalBtn.style.transform = `translateX(${counter * 22}px)`;
+    document.body.classList.add("dark");
+  } else if (counter === 2) {
+    ovalBtn.style.transform = `translateX(${counter * 22}px)`;
+    document.body.classList.add("lilac");
+  }
 
-
+  counter++;
+});
