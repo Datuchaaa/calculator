6ec6760e9f2ef5f2183f608d9ff06b65a75b7872
Daviti Vashakmadze <vashaymadzedavit@gmail.com>
2024-01-23 23:58:47 +0400

    feat: add theme option switch  function with theme change

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const calcDisplay = document.querySelector(".calc-display");
-const numbersBtns = document.querySelectorAll(".num");
-const resetBtn = document.querySelector(".reset");
-const themeSwitchBtn = document.querySelector(".theme-switch-btn");
-const ovalBtn = document.querySelector(".oval-btn");
+const calcDisplay = document.querySelector(".cacl-display")
+const numbersBtns = document.querySelectorAll(".num")
+const resetBtn = document.querySelector(".reset")
+const themeSwitchBtn = document.querySelector(".theme-switch-btn")
+const ovalBtn = document.querySelector(".oval-btn")
 
 let currentInput = "";
-let counter = 1;
+let counter = 1
 
-numbersBtns.forEach(button => {
-  button.addEventListener("click", () => {
-    currentInput += button.textContent;
-    calcDisplay.textContent = currentInput;
+  numbersBtns.forEach(button => {
+    button.addEventListener("click", () => {
+      currentInput += button.textContent;
+      calcDisplay.textContent = currentInput;
+    });
   });
-});
 
 resetBtn.addEventListener("click", () => {
   calcDisplay.textContent = "0";
