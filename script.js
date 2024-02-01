7b06368d030c5316951f9b1eae2b6957ac16c11e
Daviti Vashakmadze <vashaymadzedavit@gmail.com>
2024-02-02 00:00:31 +0400

    feat: add max number limit to display

--- script.js
+++ script.js
@@ -28,11 +28,6 @@ document.addEventListener("DOMContentLoaded", function () {
     updateDisplay();
   };
 
-
-  
-
-  
-
   numbersBtns.forEach((button) => {
     button.addEventListener("click", () => {
       handleNumberClick(button);
